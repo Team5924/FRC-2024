e25dd050306df533414a6de3c1bae7a7a6d33e9c
Michael Cole <mgbcsw13@gmail.com>
2024-01-27 15:43:45 -0800

    fix can id (quinn needs to decide laser id)

--- src/main/java/org/first5924/frc2024/constants/FeederConstants.java
+++ src/main/java/org/first5924/frc2024/constants/FeederConstants.java
@@ -6,5 +6,10 @@ package org.first5924.frc2024.constants;
 
 /** Add your docs here. */
 public class FeederConstants {
-    public static final int motorID = 1;
+    public static final int motorID = 15;
+    //for quinn to decide
+    public static final int laserID = 2;
+    public static final double distanceToNoteFromLaser = 0.0;
+    public static final double slowPercent = -0.3;
+    public static final double shootPercent = 1;
 }
