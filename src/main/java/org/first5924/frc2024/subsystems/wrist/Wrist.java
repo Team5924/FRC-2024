e1b9b194d6487fcdd73ae5e9da8abfb22178c7fc
Michael Cole <mgbcsw13@gmail.com>
2024-01-27 19:25:45 -0800

    wrist no commands

---
+++ src/main/java/org/first5924/frc2024/subsystems/wrist/Wrist.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.first5924.frc2024.subsystems.wrist;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Wrist extends SubsystemBase {
+  /** Creates a new Wrist. */
+  private final WristIO io;
+  private final WristIOInputsAutoLogged inputs = new WristIOInputsAutoLogged();
+
+  public Wrist(WristIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Wrist", inputs);
+    // This method will be called once per scheduler run
+  }
+  
+  public double getMotorTempCelsius() {
+    return inputs.motorTempCelsius;
+  }
+
+    public double getUpperMotorCurrentAmps() {
+    return inputs.motorCurrentAmps;
+  }
+
+    public double getMotorCurrentVelocity() {
+    return inputs.motorCurrentVelocity;
+  }
+
+  public void setPercent(double percent) {
+    io.setPercent(percent);
+  }
+
+  
+}
---
+++ src/main/java/org/first5924/frc2024/subsystems/wrist/WristIOTalonFX.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.first5924.frc2024.subsystems.wrist;
+
+import org.first5924.frc2024.constants.WristConstants;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+/** Add your docs here. */
+public class WristIOTalonFX implements WristIO {
+    //private final CANSparkMax mLeaderSpark = SparkMaxFactory.createSparkMax(PivotConstants.kLeaderSparkPort, MotorType.kBrushless, IdleMode.kBrake, 42);
+    private final TalonFX mMotor = new TalonFX(WristConstants.motorID);
+    private final CANcoder mEncoder = new CANcoder(WristConstants.encoderID);
+    public WristIOTalonFX() {
+    
+    }
+
+    @Override
+    public void updateInputs(WristIOInputs inputs) {
+        inputs.motorTempCelsius = mMotor.getDeviceTemp().getValueAsDouble();
+        inputs.motorCurrentAmps = mMotor.getSupplyCurrent().getValueAsDouble();
+        inputs.motorCurrentVelocity = mMotor.getVelocity().getValueAsDouble();
+        inputs.encoderPosition = mEncoder.getPosition().getValueAsDouble();
+        inputs.wristAngle = mEncoder.getPosition().getValueAsDouble()/360;
+    }
+
+    @Override
+    public void setPercent(double percent) {
+        mMotor.set(percent);
+    }
+}
