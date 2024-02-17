add1fe6cc3bb2621a16e175c279233273df66880
stretch07 <78528552+stretch07@users.noreply.github.com>
2024-02-17 15:09:01 -0800

    feat(vision): add alphabot vision capabilities
    Co-authored-by: Daniel :) <Danielk5924@users.noreply.github.com>

---
+++ src/main/java/org/first5924/frc2024/constants/FieldConstants.java
@@ -0,0 +1,17 @@
+package org.first5924.frc2024.constants;
+
+public class FieldConstants {
+    //x coordinate of the back of the speaker looking at the field from the judge's table
+    public static final double xRedSpeakerMeters = (326.61-9.8)/39.37; 
+    public static final double xBlueSpeakerMeters = -326.61/39.37;
+    
+    //y coordinate of the speakers looking at the field from the judge's table
+    public static final double ySpeakerMeters = 57/39.37;
+
+    //this value is to be tested
+    public static final double shooterExitVeloMPS = 32.6;
+    public static final double exampleShooterHeight = .7;
+
+    public static final double speakerHeight = 2.0828041656;
+}
+
---
+++ src/main/java/org/first5924/frc2024/subsystems/vision/FieldCam.java
@@ -0,0 +1,84 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.first5924.frc2024.subsystems.vision;
+
+import org.first5924.frc2024.constants.FieldConstants;
+import org.first5924.frc2024.constants.VisionConstants;
+import org.first5924.frc2024.subsystems.vision.LimelightHelpers.LimelightResults;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class FieldCam extends SubsystemBase {
+  NetworkTable botPoseTable = NetworkTableInstance.getDefault().getTable(VisionConstants.aprilTagLimelightName);
+
+  private double xBotPose = -1;
+  private double yBotPose = -1;
+
+    LimelightResults llresults;
+  public FieldCam() {}
+
+  @Override
+  public void periodic() {
+    double[] botPoseArray = botPoseTable.getEntry("botpose").getDoubleArray(new double[6]);
+    xBotPose = botPoseArray[0];
+    yBotPose = botPoseArray[1];
+    SmartDashboard.putNumber("Bot Pose X", xBotPose);
+    SmartDashboard.putNumber("Bot Pose y", yBotPose);
+    
+  }
+
+  public double getBotPoseX(){
+    return xBotPose;
+  }
+
+  public double getBotPoseY(){
+    return yBotPose;
+  }
+
+  public double getDistanceToBlueSpeakerMeters(){
+    double x = FieldConstants.xBlueSpeakerMeters - xBotPose;
+    double y = FieldConstants.ySpeakerMeters - yBotPose;
+
+    //pythagorean theorem to return distance to speaker as the crow flies
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+  }
+
+  public double getDistanceToRedSpeakerMeters(){
+    double x = FieldConstants.xRedSpeakerMeters - xBotPose;
+    double y = FieldConstants.ySpeakerMeters - yBotPose;
+
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  }
+
+  public double getRedShooterAngle(){
+
+    // estimate the amount that the note will drop due to gravity using Δx = 1/2 * a * t^2
+    double time = getDistanceToRedSpeakerMeters()/FieldConstants.shooterExitVeloMPS;
+    double drop = .5 * -9.81 * Math.pow(time, 2);
+
+    // set the x and y values for the arctan
+    double y = FieldConstants.speakerHeight - FieldConstants.exampleShooterHeight + drop;
+    double x = getDistanceToRedSpeakerMeters();
+    return Math.atan(y/x);
+  }
+
+  public double getBlueShooterAngle(){
+
+    // estimate the amount that the note will drop due to gravity using Δx = 1/2 * a * t^2
+    double time = getDistanceToBlueSpeakerMeters()/FieldConstants.shooterExitVeloMPS;
+    double drop = .5 * -9.81 * Math.pow(time, 2);
+
+    // set the x and y values for the arctan
+    double y = FieldConstants.speakerHeight - FieldConstants.exampleShooterHeight + drop;
+    double x = getDistanceToBlueSpeakerMeters();
+    return Math.atan(y/x);
+  }
+  
+
+}
