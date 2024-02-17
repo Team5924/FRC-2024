5a55da48bc5619a3655bd4e953d56a6ca66f3a75
James Guleno <jguleno27@siprep.org>
2024-02-17 14:24:43 -0800

    adjust slow multipliers

--- src/main/java/org/first5924/frc2024/constants/DriveConstants.java
+++ src/main/java/org/first5924/frc2024/constants/DriveConstants.java
@@ -49,6 +49,6 @@ public class DriveConstants {
   public static final double kEncoderToDriveRatio = 5.14;
   public static final double kEncoderToTurnRatio = 396/35;
 
-  public static final double kSlowModeMovementMultiplier = 0.5;
-  public static final double kSlowModeRotationMultiplier = 1;
+  public static final double kSlowModeMovementMultiplier = 0.25;
+  public static final double kSlowModeRotationMultiplier = 0.25;
 }
